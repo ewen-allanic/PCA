66b09a4f045394b42b19ec4e00542896c3704a6f
Ewen Allanic <51999185+ewen-allanic@users.noreply.github.com>
2020-11-10 11:37:16 +0100

    node

---
+++ tableur/node.c
@@ -0,0 +1,20 @@
+struct node {
+    void * data;
+    struct node * suivant;
+}
+typedef struct node node_t;
+
+node_t * list_create(void) {
+    return (node_t *)NULL;
+}
+
+void * list_get_data(const node_t * node) {
+    return node->data;
+}
+void list_set_data(node_t * node, void * data) {
+    node->data = data;
+}
+
+node_t * list_next(note_t * node, void * data) {
+
+}
